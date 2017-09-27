e4f5b027ce7bf91c71e7c82752791481563b928f
unknown <chuwenbo86@163.com>
2017-09-27 00:43:35 -0700

    update local repo folder test

--- personal-website/server.js
+++ personal-website/server.js
@@ -4,5 +4,5 @@ var port = process.env.PORT || 1337;
 
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Wenbo Chu\n');
+    res.end('Wenbo Chu -- Welcome\n');
 }).listen(port);
