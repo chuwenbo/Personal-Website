98fc2af127dfa6ca4bbfda3c49e15f6ca9053550
unknown <chuwenbo86@163.com>
2017-09-28 00:28:52 -0700

    install express framework

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
-'use strict';
-var http = require('http');
-var port = process.env.PORT || 1337;
+'use strict'; 
 
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Wenbo Chu -- Welcome\n');
-}).listen(port);
+var port = process.env.PORT || 1337; 
+var http = require('http');
+var express = require('express');
+var ejs = require('ejs');
+const path = require('path');
+const app = express(); 
+
+
+//set up view engine
+app.engine('.html', ejs.__express);
+app.set('views', __dirname + '/app/views');
+app.set('view engine', 'html'); 
+
+//serve static files as a express middleware
+app.use(express.static(path.join(__dirname, 'app/public')));
+
+
+app.get('/', function (req, res) {
+    res.render('index');
+});
+
+app.listen(port, function () {
+    console.log('Example app listening on port 3000!')
+});
